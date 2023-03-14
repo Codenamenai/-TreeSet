3ea97db750eae9cd2f76390e57a1851861ee9721
Anton Nazarov <antonionazarini@yandex.ru>
2023-03-14 20:17:34 +0300

    lambda

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +11,19 @@ public class Main {
         noble.add(new Person("Alan", "Kempebericky", 39));
         noble.add(new Person("Edvard", "von Baron", 60));
         noble.add(new Person("Andrej", "Petrov Vodkin", 56));
-        Collections.sort(noble, new PersonComparator());
+        Comparator<Person>comparator=(Person o1,Person o2) ->{
+            String[] surname1 = o1.getSurname().split("\\P{IsAlphabetic}+");
+            String[] surname2 = o2.getSurname().split("\\P{IsAlphabetic}+");
+            int surnameLength1 = Math.min(surname1.length, 2);
+            int surnameLength2 = Math.min(surname2.length, 2);
+            int surnameCompare = Integer.compare(surnameLength2, surnameLength1);
+            if (surnameCompare == 0) {
+                return Integer.compare(o2.getAge(), o1.getAge());
+            } else {
+                return surnameCompare;
+            }
+        };
+        Collections.sort(noble, comparator);
         System.out.println(noble);
 
 
