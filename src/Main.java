bf219b6dbad89ec68197c482617a6081f902cb7d
Anton Nazarov <antonionazarini@yandex.ru>
2023-03-16 13:55:14 +0300

    lambda2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Comparator;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,16 +12,19 @@ public class Main {
         noble.add(new Person("Alan", "Kempebericky", 39));
         noble.add(new Person("Edvard", "von Baron", 60));
         noble.add(new Person("Andrej", "Petrov Vodkin", 56));
+       /* noble.add(new Person("Trifon","Barmaleev",17));
+        noble.add(new Person("Ulugbek","Tohtamischev",16));
+        noble.removeIf(x->x.getAge()<18);*/
         Comparator<Person>comparator=(Person o1,Person o2) ->{
             String[] surname1 = o1.getSurname().split("\\P{IsAlphabetic}+");
             String[] surname2 = o2.getSurname().split("\\P{IsAlphabetic}+");
-            int surnameLength1 = Math.min(surname1.length, 2);
-            int surnameLength2 = Math.min(surname2.length, 2);
-            int surnameCompare = Integer.compare(surnameLength2, surnameLength1);
-            if (surnameCompare == 0) {
+
+            int compare = Integer.compare(surname2.length,
+                    surname1.length);
+            if (compare == 0) {
                 return Integer.compare(o2.getAge(), o1.getAge());
             } else {
-                return surnameCompare;
+                return compare;
             }
         };
         Collections.sort(noble, comparator);
